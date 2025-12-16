726870c92b937f71b58885abdf9f6b1576cd7745
Alena Volkova <grafinya.vishenka9@yandex.ru>
2025-12-16 18:10:51 +0300

    parametrize_function_in_test_constructor_py

--- tests/test_constructor.py
+++ tests/test_constructor.py
@@ -4,33 +4,26 @@ from credentials import Credentials
 from locators import Locators
 from url import *
 import time
+import pytest
 
 class TestConstructor:
 
-    def test_constructor_move_to_sousy(self, driver_login):
-        driver_login.find_element(*Locators.MOVE_TO_SOUSY).click()
+    @pytest.mark.parametrize(
+        'locators_start, locators_end, locators_verify, ingridient',#locators_start - переключаем на любой пункт меню, кроме тестируемого
+                                                                    #locators_end - переключаем на тестируемый пункт меню
+                                                                    # сделано по причине некликабельности пункта меню по умолчанию(Булки) 
+        [
+            [Locators.MOVE_TO_SOUSY, Locators.MOVE_TO_BULKI, Locators.MOVE_TO_BULKI_VERIFY, 'Булки'],
+            [Locators.MOVE_TO_NACHINKI, Locators.MOVE_TO_SOUSY, Locators.MOVE_TO_SOUSY_VERIFY, 'Соусы'],            
+            [Locators.MOVE_TO_SOUSY, Locators.MOVE_TO_NACHINKI, Locators.MOVE_TO_NACHINKI_VERIFY, 'Начинки']
+        ]
+    )
+    def test_constructor(self, driver_login, locators_start, locators_end, locators_verify, ingridient):
+        driver_login.find_element(*locators_start).click()
+        driver_login.find_element(*locators_end).click()
         time.sleep(3)
-        reg_text = WebDriverWait(driver_login, 10, poll_frequency=0.1).until(EC.visibility_of_element_located(Locators.MOVE_TO_SOUSY)).text
-        is_active = WebDriverWait(driver_login, 10, poll_frequency=0.1).until(EC.visibility_of_element_located(Locators.MOVE_TO_SOUSY_VERIFY)).get_attribute("class")
-        assert 'Соусы' == reg_text
-        assert "tab_tab_type_current__2BEPc" in is_active
-        assert driver_login.current_url == main_site
-
-    def test_constructor_move_to_bulki(self, driver_login):
-        driver_login.find_element(*Locators.MOVE_TO_NACHINKI).click()
-        driver_login.find_element(*Locators.MOVE_TO_BULKI).click()
-        time.sleep(3)
-        reg_text = WebDriverWait(driver_login, 10, poll_frequency=0.1).until(EC.visibility_of_element_located(Locators.MOVE_TO_BULKI)).text
-        is_active = WebDriverWait(driver_login, 10, poll_frequency=0.1).until(EC.visibility_of_element_located(Locators.MOVE_TO_BULKI_VERIFY)).get_attribute("class")
-        assert 'Булки' == reg_text
-        assert "tab_tab_type_current__2BEPc" in is_active
-        assert driver_login.current_url == main_site
-
-    def test_constructor_move_to_nachinki(self, driver_login):
-        driver_login.find_element(*Locators.MOVE_TO_NACHINKI).click()
-        time.sleep(3)
-        reg_text = WebDriverWait(driver_login, 10, poll_frequency=0.1).until(EC.visibility_of_element_located(Locators.MOVE_TO_NACHINKI)).text
-        is_active = WebDriverWait(driver_login, 10, poll_frequency=0.1).until(EC.visibility_of_element_located(Locators.MOVE_TO_NACHINKI_VERIFY)).get_attribute("class")
-        assert 'Начинки' == reg_text
+        reg_text = WebDriverWait(driver_login, 10, poll_frequency=0.1).until(EC.visibility_of_element_located(locators_end)).text
+        is_active = WebDriverWait(driver_login, 10, poll_frequency=0.1).until(EC.visibility_of_element_located(locators_verify)).get_attribute("class")
+        assert ingridient == reg_text
         assert "tab_tab_type_current__2BEPc" in is_active
         assert driver_login.current_url == main_site
